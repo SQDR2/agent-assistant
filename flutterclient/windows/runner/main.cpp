4d3ce9cbc8424de4bb15ecbf42a366c7cb05895e
SQDR2 <1842181703@qq.com>
2025-06-15 12:43:51 +0800

    fix: add support for launching agentassistant-srv.exe on Windows startup

--- flutterclient/windows/runner/main.cpp
+++ flutterclient/windows/runner/main.cpp
@@ -1,21 +1,40 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <Shlwapi.h> // For PathRemoveFileSpecW
 
 #include "flutter_window.h"
 #include "utils.h"
 
+// Link with Shlwapi.lib
+#pragma comment(lib, "Shlwapi.lib")
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+                      _In_ wchar_t *command_line, _In_ int show_command)
+{
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
+  {
     CreateAndAttachConsole();
   }
 
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+  // --- Start agentassistant-srv.exe ---
+  wchar_t module_path[MAX_PATH];
+  GetModuleFileNameW(nullptr, module_path, MAX_PATH);
+  PathRemoveFileSpecW(module_path); // Now module_path contains the directory of the executable
+
+  // Construct the full path to agentassistant-srv.exe
+  wchar_t srv_path[MAX_PATH];
+  swprintf_s(srv_path, MAX_PATH, L"%s\\agentassistant-srv.exe", module_path);
+
+  // Launch agentassistant-srv.exe in a hidden window
+  ShellExecuteW(nullptr, L"open", srv_path, nullptr, nullptr, SW_HIDE);
+  // --- End agentassistant-srv.exe startup ---
 
   flutter::DartProject project(L"data");
 
@@ -27,13 +46,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"flutterclient", origin, size)) {
+  if (!window.Create(L"flutterclient", origin, size))
+  {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
 
   ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
+  while (::GetMessage(&msg, nullptr, 0, 0))
+  {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
